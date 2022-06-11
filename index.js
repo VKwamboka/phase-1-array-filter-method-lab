011f8c86357446918280328b144d6b6e4ec7ed21
verah <verahnyagaka101@gmail.com>
2022-06-11 21:52:36 +0300

    commit tree cefbdce72ac9fd5f867e4b2edafa785f3354786e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function  findMatching(arrayDrivers,string){
+    return arrayDrivers.filter(
+        (match) => match.toLowerCase() === string.toLowerCase() );
+}
+
+function fuzzyMatch(arrayDrivers,string){
+    return arrayDrivers.filter(
+        (match) => match.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function  matchName(arrayDrivers,string){
+    return arrayDrivers.filter((data) => data.name === string);
+}
